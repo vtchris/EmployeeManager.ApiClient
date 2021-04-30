697ddf3faabe01186420fd8fb5530b94d99029f4
vtchris <vtchris.2357@gmail.com>
2021-04-30 11:24:40 -0400

    70: HttpClient, API Client countries list

---
+++ Controllers/EmployeeManagerController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using EmployeeManager.ApiClient.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
+
+namespace EmployeeManager.ApiClient.Controllers
+{
+    [Authorize(Roles = "Manager")]
+    public class EmployeeManagerController : Controller
+    {
+        private readonly HttpClient client = null;
+        private string employeesApiUrl = null;
+
+        public EmployeeManagerController(HttpClient client, IConfiguration config)
+        {
+            this.client = client;
+            employeesApiUrl = config.GetValue<string>("AppSettings:EmployeesApiUrl");
+        }
+
+        public async Task<bool> FillCountriesAsync()
+        {
+            HttpResponseMessage response = await client.GetAsync(employeesApiUrl);
+            string stringData = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var countries = JsonSerializer.Deserialize<List<Employee>>(stringData, options).Select(c =>
+            c.Country).Distinct();
+
+            List<SelectListItem> listOfCountries = new SelectList(countries).ToList();
+            ViewBag.Countries = listOfCountries;
+            return true;
+        }
+    }
+}
